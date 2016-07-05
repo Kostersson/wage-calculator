c7ae206399b7020e4ac76fb80f6a25ba6b8b4618
Pauli Kostamo <pauli.kostamo@w3.fi>
2016-07-05 18:52:56 +0300

    refactor map to iterable

--- src/app/pipes/map-to-iterable-pipe.ts
+++ src/app/pipes/map-to-iterable-pipe.ts
@@ -1,19 +1,15 @@
 import {Pipe, PipeTransform} from '@angular/core';
 
-@Pipe({ name: 'mapToIterable' })
+@Pipe({name: 'mapToIterable'})
 export class MapToIterable {
   transform(value) {
     let result = [];
 
-    if(value.entries) {
-      for (var [key, value] of value.entries()) {
-        result.push({ key, value });
-      }
-    } else {
-      for(let key in value) {
-        result.push({ key, value: value[key] });
-      }
+    for (var [key, value] of value.entries()) {
+      result.push({key, value});
     }
+
+
     return result;
   }
 }
